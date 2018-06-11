7a032aed6b4a112f8683998a7bb8af4e95f5b0be
Scott Fasone <scott181182@yahoo.com>
2018-06-11 18:20:35 -0400

    Added Handled Lifecycle Nodes

--- src/mwave/mwave_util/include/mwave_util/nodes.hpp
+++ src/mwave/mwave_util/include/mwave_util/nodes.hpp
@@ -2,6 +2,7 @@
 #define MWAVEUTIL_HANDLEDNODE_HPP
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
 
 #include <memory>
 #include <vector>
@@ -15,6 +16,51 @@ namespace mwave_util
             using SharedPtr = std::shared_ptr<HandledNode>;
             
             explicit HandledNode(const std::string & node_name);
+            virtual void start();
+
+            template<typename MessageT, typename Alloc = std::allocator<void>>
+            void add_publisher(const std::string & topic,
+                const rmw_qos_profile_t & qos_profile = rmw_qos_profile_default,
+                std::shared_ptr<Alloc> allocator = nullptr);
+
+            template<
+                typename MessageT,
+                typename Alloc = std::allocator<void>,
+                typename PublisherT = ::rclcpp::Publisher<MessageT, Alloc>>
+            std::shared_ptr<PublisherT> get_publisher(const std::string & topic);
+
+            template<
+                typename MessageT,
+                typename CallbackT,
+                typename Alloc = std::allocator<void>,
+                typename SubscriptionT = rclcpp::Subscription<MessageT, Alloc>>
+            std::shared_ptr<SubscriptionT> add_subscription(
+                const std::string & topic_name,
+                CallbackT && callback,
+                const rmw_qos_profile_t & qos_profile = rmw_qos_profile_default,
+                rclcpp::callback_group::CallbackGroup::SharedPtr group = nullptr,
+                bool ignore_local_publications = false,
+                typename rclcpp::message_memory_strategy::MessageMemoryStrategy<MessageT, Alloc>::SharedPtr
+                msg_mem_strat = nullptr,
+                std::shared_ptr<Alloc> allocator = nullptr);
+
+            template<typename DurationT = std::milli, typename CallbackT>
+            typename rclcpp::WallTimer<CallbackT>::SharedPtr
+            add_wall_timer(std::chrono::duration<int64_t, DurationT> period, CallbackT callback,
+                rclcpp::callback_group::CallbackGroup::SharedPtr group = nullptr);
+
+        private:
+            std::map<std::string, rclcpp::PublisherBase::SharedPtr> publishers;
+            std::vector<rclcpp::SubscriptionBase::SharedPtr> subscriptions;
+            std::vector<rclcpp::TimerBase::SharedPtr> timers;
+    };
+
+    class HandledLifecycleNode : public rclcpp_lifecycle::LifecycleNode
+    {
+        public:
+            using SharedPtr = std::shared_ptr<HandledLifecycleNode>;
+            
+            explicit HandledLifecycleNode(const std::string & node_name);
             virtual void start();
 
             template<typename MessageT, typename Alloc = std::allocator<void>>
--- src/mwave/mwave_util/src/mwave_util/nodes.cpp
+++ src/mwave/mwave_util/src/mwave_util/nodes.cpp
@@ -47,4 +47,54 @@ namespace mwave_util
     	this->timers.push_back(ret);
     	return ret;
     }
+
+
+
+
+
+    HandledLifecycleNode::HandledLifecycleNode(const std::string & node_name) : LifecycleNode(node_name) {  }
+    void HandledLifecycleNode::start()
+    {
+        RCLCPP_INFO(this->get_logger(), "Started node '%s'", this->get_name());
+    }
+
+    template<typename MessageT, typename Alloc>
+    void HandledLifecycleNode::add_publisher(const std::string & topic,
+        const rmw_qos_profile_t & qos_profile,
+        std::shared_ptr<Alloc> allocator)
+    {
+    	auto ret = this->create_publisher<MessageT>(topic, qos_profile, allocator);
+    	this->publishers[topic] = ret;
+    }
+    template<typename MessageT, typename Alloc, typename PublisherT>
+    std::shared_ptr<PublisherT> HandledLifecycleNode::get_publisher(const std::string & topic)
+    {
+    	return this->publishers.at(topic);
+    }
+
+
+    template<typename MessageT, typename CallbackT, typename Alloc, typename SubscriptionT>
+    std::shared_ptr<SubscriptionT> HandledLifecycleNode::add_subscription(
+        const std::string & topic_name,
+        CallbackT && callback,
+        const rmw_qos_profile_t & qos_profile,
+        rclcpp::callback_group::CallbackGroup::SharedPtr group,
+        bool ignore_local_publications,
+        typename rclcpp::message_memory_strategy::MessageMemoryStrategy<MessageT, Alloc>::SharedPtr msg_mem_strat,
+        std::shared_ptr<Alloc> allocator)
+    {
+    	auto ret = this->create_subscription(topic_name, callback, qos_profile, group, ignore_local_publications, msg_mem_strat, allocator);
+    	this->subscriptions.push_back(ret);
+    	return ret;
+    }
+
+    template<typename DurationT, typename CallbackT>
+    typename rclcpp::WallTimer<CallbackT>::SharedPtr
+    HandledLifecycleNode::add_wall_timer(std::chrono::duration<int64_t, DurationT> period, CallbackT callback,
+        rclcpp::callback_group::CallbackGroup::SharedPtr group)
+    {
+    	auto ret = this->create_wall_timer(period, callback, group);
+    	this->timers.push_back(ret);
+    	return ret;
+    }
 }
