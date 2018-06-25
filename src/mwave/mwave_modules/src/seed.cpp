5de076ace22ef28197467d329a066aa1abbc8998
Nolan Rosen <nolanrosen15@gmail.com>
2018-06-25 17:09:02 -0400

    Seed Node Rework

--- src/mwave/mwave_modules/src/seed.cpp
+++ src/mwave/mwave_modules/src/seed.cpp
@@ -11,31 +11,44 @@
 #include "mwave_messages/srv/fetch_config_type.hpp"
 
 #include "rclcpp/rclcpp.hpp"
+	
+/*
+rclcpp::Client<mwave_messages::srv::FetchConfigType>::SharedResponse result;
+RCLCPP_INFO(this->get_logger(), "Handling Future.");
+//if(future_result.valid()) { RCLCPP_INFO(this->get_logger(), "That was quick."); }
+//else {
+	RCLCPP_INFO(this->get_logger(), "Waiting...");
+	future_result.wait();
+	RCLCPP_INFO(this->get_logger(), "Ready!");
+//}
+result = future_result.get();
+RCLCPP_INFO(this->get_logger(), "Handling Response.");
 
-class SeedNode : public rclcpp::Node
+if(result->error.size() > 0) {
+	RCLCPP_ERROR(this->get_logger(), "Well Crap, error on config fetch.");
+	return;
+}
+
+*/
+//});
+/*
+mwave_messages::srv::FetchConfigType::Response::SharedPtr fetch_config()
 {
-	public:
-		explicit SeedNode(const std::string & name, rclcpp::executors::SingleThreadedExecutor & exec) : rclcpp::Node(name)
-		{
-			rclcpp::Client<mwave_messages::srv::FetchConfigType>::SharedPtr config_client = 
-				this->create_client<mwave_messages::srv::FetchConfigType>("/FetchConfigType");
+	rclcpp::Client<mwave_messages::srv::FetchConfigType>::SharedPtr config_client = 
+		this->create_client<mwave_messages::srv::FetchConfigType>("/FetchConfigType");
 
-			auto config_request = std::make_shared<mwave_messages::srv::FetchConfigType::Request>();
-			config_request->node_name = name;
-			
-			auto future = config_client->async_send_request(config_request);
-			auto result = future.get();
-			for (std::string type : result->nodes) {
-				if (type == "ion") {
-					exec.add_node(std::make_shared<mwave_modules::IOComponent>(name + "_ion"));				
-				} else if (type == "log") {
-					exec.add_node(std::make_shared<mwave_modules::LogicComponent>(name + "_log"));
-				} else if (type == "hmi") {
-					exec.add_node(std::make_shared<mwave_modules::HMIComponent>(name + "_hmi"));
-				}			
-			}
-		}
-};
+	auto config_request = std::make_shared<mwave_messages::srv::FetchConfigType::Request>();
+	config_request->node_name = this->get_name();
+
+	auto future_result = config_client->async_send_request(config_request);
+
+	if(rclcpp::spin_until_future_complete(this, future_result) == rclcpp::executor::FutureReturnCode::SUCCESS) {
+		return future_result.get();
+	} else {
+		return NULL;
+	}
+}
+*/
 
 int main(int argc, char * argv[])
 {
@@ -46,12 +59,34 @@ int main(int argc, char * argv[])
 
 	// TODO: Create a config file to load this in from
 	std::string name = "testall";
+
+	// TODO: Make this a mutlithreaded executor
 	rclcpp::executors::SingleThreadedExecutor exec;
-	
-	auto seed_node = std::make_shared<SeedNode>(name, exec);
-		
+	auto seed_node = std::make_shared<rclcpp::Node>(name);
+	// Get config for exec
+	// spin till future
+
+	rclcpp::Client<mwave_messages::srv::FetchConfigType>::SharedPtr config_client = 
+				seed_node->create_client<mwave_messages::srv::FetchConfigType>("/FetchConfigType");
+
+	auto config_request = std::make_shared<mwave_messages::srv::FetchConfigType::Request>();
+	config_request->node_name = name;
+
+	auto future_result = config_client->async_send_request(config_request, [&name, &exec](rclcpp::Client<mwave_messages::srv::FetchConfigType>::SharedFuture future) -> void {
+		for(std::string type : future.get()->nodes) {
+			if (type == "ion") {
+				exec.add_node(std::make_shared<mwave_modules::IOComponent>(name + "_ion"));				
+			} else if (type == "log") {
+				exec.add_node(std::make_shared<mwave_modules::LogicComponent>(name + "_log"));
+			} else if (type == "hmi") {
+				exec.add_node(std::make_shared<mwave_modules::HMIComponent>(name + "_hmi"));
+			}			
+		}
+		exec.remove_node(seed_node);
+	});
+
+	exec.add_node(seed_node);
     exec.spin();
-	
     rclcpp::shutdown();
 	
     return 0;
--- src/mwave/mwave_orchestrator/src/services/mariadb.cpp
+++ src/mwave/mwave_orchestrator/src/services/mariadb.cpp
@@ -42,7 +42,7 @@ class OrchestratorConfigNode : public rclcpp::Node
                 RCLCPP_ERROR(this->get_logger(), "HOLY SHIT DATABASE WONT CONNECT!");
                 exit(1);
             }
-            this->query_nodes      = sql_connection->create_statement("SELECT * FROM nodes WHERE slug=?;");
+            this->query_node_types = sql_connection->create_statement("SELECT * FROM node_types WHERE node=?;");
             this->query_config_i2c = sql_connection->create_statement("SELECT * FROM i2c_config WHERE node=?;");
             this->query_topics_i2c = sql_connection->create_statement("SELECT pin, topic FROM i2c_topics WHERE node=? AND bus=? AND address=?;");
 
@@ -54,24 +54,19 @@ class OrchestratorConfigNode : public rclcpp::Node
             {
                 (void)request_header;
                 RCLCPP_INFO(this->get_logger(), "Request of type for node: %s", request->node_name.c_str());
-                response->error = "";
-		        std::vector<std::string> ret_nodes = std::vector<std::string>();
-		
-        		// TODO: Change the database to provide a list of nodes (comma seperated) given a node name.
-		        // 	 This change can be in the ros2config:nodes table.
-
-                query_nodes->set_string(0, request->node_name);
-                auto sql_result = query_nodes->query();
-
-                if (sql_result->next()) {
-			        // TODO: See above 'todo'
-               		//response->nodes = split( sql_result->get_string("type") );
-                } else {
-                    RCLCPP_WARN(this->get_logger(), "No SQL Results");
-                    response->error = "Node name is not recognized";
+                // Get types from node_types
+                query_node_types->set_string(0, request->node_name);
+                auto sql_result = query_node_types->query();
+                // Populate ret_nodes with type names
+                std::vector<std::string> ret_nodes = std::vector<std::string>();
+                while (sql_result->next()) {
+                    std::string type_name = sql_result->get_string("type");
+                    RCLCPP_INFO(this->get_logger(), "Types: %s", type_name.c_str());
+                    ret_nodes.push_back(type_name);
                 }
-		
-		        response->error = "This has not been implemented!";
+
+                response->nodes = ret_nodes;
+		        response->error = "";
             };
             //A callback function for when nodes configure thier input and output devices. (Message Type: FetchIOConfig)
             auto handle_fetch_io_config = 
@@ -81,24 +76,23 @@ class OrchestratorConfigNode : public rclcpp::Node
             {
                 (void)request_header;
                 RCLCPP_INFO(this->get_logger(), "Request of input/output for node: %s", request->node.c_str());
-
+                // Get bus, address, device, option_keys, and option_values from i2c_config table
                 query_config_i2c->set_string(0, request->node);
                 auto sql_result = query_config_i2c->query();
-                
+                // Populate transient object with data from i2c_config
                 std::vector<mwave_messages::msg::I2CDevice> ret_devices;
-                
                 while(sql_result->next()) {
                     auto device = this->parseDevice(sql_result);
                     ret_devices.push_back(*device);
                 }
-
+                // Construct the response from the transient objects
                 std::vector<mwave_messages::msg::I2CDevice>::iterator itr;
                 for(itr = ret_devices.begin(); itr != ret_devices.end(); itr++) {
                     query_topics_i2c->set_string(0, request->node);
                     query_topics_i2c->set_unsigned8(1, itr->bus);
                     query_topics_i2c->set_unsigned8(2, itr->address);
+                    // Reassign sql_result to table i2c_topics matching name, bus, and address
                     sql_result = query_topics_i2c->query();
-
                     itr->topics.resize(sql_result->row_count());
                     while(sql_result->next()) {
                         itr->topics.at(sql_result->get_unsigned8("pin")) = sql_result->get_string("slug");
@@ -124,7 +118,7 @@ class OrchestratorConfigNode : public rclcpp::Node
                     response->dummy_response = "Dummy Response.";
                 };            
             
-            srv_fetch_config_type = create_service<mwave_messages::srv::FetchConfigType>("FetchConfigType", handle_fetch_config_type);
+            srv_fetch_config_type = create_service<mwave_messages::srv::FetchConfigType>("/FetchConfigType", handle_fetch_config_type);
             srv_fetch_io_config = create_service<mwave_messages::srv::FetchIOConfig>("FetchIOConfig", handle_fetch_io_config);
             srv_fetch_hmi_config = create_service<mwave_messages::srv::FetchHMIConfig>("FetchHMIConfig", handle_fetch_hmi_config);
             srv_fetch_logic_config = create_service<mwave_messages::srv::FetchLogicConfig>("FetchLogicConfig", handle_fetch_logic_config);
@@ -132,6 +126,9 @@ class OrchestratorConfigNode : public rclcpp::Node
             RCLCPP_INFO(this->get_logger(), "Listening!");
         };
 
+        /*
+         * Given a result set from the table i2c_config, this method will return a parsed version as a (I2CDevice) message.
+         */
         std::shared_ptr<mwave_messages::msg::I2CDevice> parseDevice(const std::shared_ptr<mariadb::result_set>& sql_result)
         {
             mwave_messages::msg::I2CDevice config;
@@ -160,7 +157,7 @@ class OrchestratorConfigNode : public rclcpp::Node
             std::shared_ptr<mariadb::account> sql_account = mariadb::account::create("127.0.0.1", "ros2", "oCXxFFUmBbVbV3gTM35DaTYveA4Ahh2P", "ros2config");
             std::shared_ptr<mariadb::connection> sql_connection = mariadb::connection::create(sql_account);
 
-            std::shared_ptr<mariadb::statement> query_nodes;
+            std::shared_ptr<mariadb::statement> query_node_types;
             std::shared_ptr<mariadb::statement> query_config_i2c;
             std::shared_ptr<mariadb::statement> query_topics_i2c;
 };
